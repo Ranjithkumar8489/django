89fcc0b1c678a2c425657a9eb1388bb316f403c9
Ranjithkumar <48977653+Ranjithkumar8489@users.noreply.github.com>
2024-06-25 23:57:15 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('arbitrage/', views.arbitrage_bot_view, name='arbitrage_bot'),
-    path('stop-bot/', views.stop_bot_view, name='stop_bot'),
-]
