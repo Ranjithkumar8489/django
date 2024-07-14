e0cdf72c4b88aed3c69e99b2c1a0205c4ff0dc11
Ranjithkumar <48977653+Ranjithkumar8489@users.noreply.github.com>
2024-07-14 13:39:35 +0530

    Delete templates directory

--- templates/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('arbitrage/', views.arbitrage_view, name='arbitrage'),
-    # Add other URL patterns as needed
-]
--- templates/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from .arbitrage import arbitrage
-
-def arbitrage_view(request):
-    symbol = "TRX/USDT"
-    amount = 60.00
-
-    result = arbitrage(symbol, amount,exchanges=["binance","kucoin"])
-    return HttpResponse(result)
